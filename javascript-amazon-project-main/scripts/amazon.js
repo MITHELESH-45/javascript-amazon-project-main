07efa92cda16838959e3155fbe3bf8afd5285bb7
Mithelesh-45 <mitheleshk3005@gmail.com>
2025-06-16 23:50:04 +0530

    Made the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -72,13 +72,23 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         })
         if(matchingItem)
         {
-            matchingItem+=1;
+            matchingItem.quantity+=1;
         }
         else{
             cart.push({
             productId: productId,
             quantity: 1
         })
-        }      
-    })
-})
+        } ;
+
+        let cartQuantity=0;
+
+        cart.forEach((item)=>{
+            cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+        .innerHTML= cartQuantity;
+            
+    });
+});
